36eaf81705f5f6bea166344a9ea44153458bcb54
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:25:10 -0600

    add primary key id to fed seed

--- migrations/20180611134925_fed_log.js
+++ migrations/20180611134925_fed_log.js
@@ -2,6 +2,7 @@
 module.exports.up = (knex, Promise) => {
   return knex.schema.createTable('fed', (table) => {
     // onDelete('cascade tells the db to delete this column in this table and the referenced table')
+    table.increments('id').primary()
     table.foreign('user_id').references('user.id').onDelete('CASCADE');
     table.foreign('pet_id').references('pet.id');
     table.timestamp('fedOn').defaultTo(knex.fn.now());
