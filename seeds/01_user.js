c2eb3e3e3eb7f7b59fd08908859a29dfaf537de1
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:09:41 -0600

    add seed user data

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex, Promise) {
+module.exports.seed = (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('user').del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('user').insert([
+        {id: 1, username: 'Bailey'},
+        {id: 2, username: 'Tim'},
+        {id: 3, username: 'Hunter'}
       ]);
     });
 };
