8e904b165cb790dc29ce3d225d86e416f7f7f817
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:47:12 -0600

    use exported queries to return data from user table

--- router/routes.js
+++ router/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const queries = require('../queries');
 
 router.get('/', (req, res, next) => {
-  res.json('hello world');
+  queries.listUsers()
+    .then(users => { res.json({ users }); });
 });
 
 module.exports = router;
