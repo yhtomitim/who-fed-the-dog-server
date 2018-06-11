65d3b94a5506a17844136d8dded2762afa5fea5e
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 12:44:34 -0600

    add seeds and migrations folders, edit knexfile for use with postgreSQL

--- knexfile.js
+++ knexfile.js
@@ -3,42 +3,13 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: 'postgres:///wftdog'
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
 
 };
