14913f3e79ccf5c2d07987a31bf7e829dd5c24c7
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 16:05:46 -0600

    add get routes for all three tables

--- router/routes.js
+++ router/routes.js
@@ -2,9 +2,28 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../queries');
 
-router.get('/', (req, res, next) => {
+router.get('/users', (req, res, next) => {
   queries.listUsers()
     .then(users => { res.json({ users }); });
 });
+
+router.get('/pets', (req, res, next) => {
+  queries.listPets()
+    .then(pets => {
+      res.json({
+        pets
+      });
+    });
+});
+
+router.get('/fedon', (req, res, next) => {
+  queries.listFedOn()
+    .then(fed => {
+      res.json({
+        fed
+      });
+    });
+});
+
 
 module.exports = router;
