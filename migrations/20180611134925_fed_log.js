ddf482c84236efe164bf2e8cc6190a6e4683439d
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:37:46 -0600

    refactor foreign keys

--- migrations/20180611134925_fed_log.js
+++ migrations/20180611134925_fed_log.js
@@ -2,9 +2,9 @@
 module.exports.up = (knex, Promise) => {
   return knex.schema.createTable('fed', (table) => {
     // onDelete('cascade tells the db to delete this column in this table and the referenced table')
-    table.increments('id').primary()
-    table.foreign('user_id').references('user.id').onDelete('CASCADE');
-    table.foreign('pet_id').references('pet.id');
+    table.increments('id').primary();
+    table.integer('user_id').references('user.id').unsigned().onDelete('CASCADE');
+    table.integer('pet_id').references('pet.id').unsigned().onDelete('CASCADE');
     table.timestamp('fedOn').defaultTo(knex.fn.now());
 
   });
@@ -13,3 +13,10 @@ module.exports.up = (knex, Promise) => {
 module.exports.down = (knex, Promise) => {
   return knex.scheme.dropTableIfExists('fed');
 };
+
+// Knex: warning - migration file "20180611134925_fed_log.js"
+// failed
+// Knex: warning - migration failed with error: alter table "fed"
+// add constraint "fed_user_id_foreign"
+// foreign key("user_id") references "user" ("id") on delete CASCADE - column "user_id"
+// referenced in foreign key constraint does not exist
