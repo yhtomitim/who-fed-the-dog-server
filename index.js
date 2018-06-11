2498c08a4ee4f22da23d65757c9a66e1660db893
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 13:14:33 -0600

    refactor app.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-require('dotenv').config();
-
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 8080;
 
-app.use(bodyParser());
-app.use(cors());
-app.use(morgan('dev'));
-
-
-app.listen(port, () => console.log(`listening on ${port}`)
-);
+app.listen(port, () => console.log(`listening on ${port}`));
