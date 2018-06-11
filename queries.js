a1fbe61aebcd041a6771bacf94a916a242f27713
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:47:36 -0600

    add quereies for listing all data in all tables

--- queries.js
+++ queries.js
@@ -1,1 +1,13 @@
 const knex = require('./connection');
+
+module.exports = {
+  listUsers() {
+    return knex('user');
+  },
+  listPets() {
+    return knex('pet');
+  },
+  listFedOn() {
+    return knex('fedOn');
+  }
+};
