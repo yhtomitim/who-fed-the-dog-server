98d96f5cbefce2707fed9793b3f35cced0c45cb1
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 13:21:26 -0600

    send response as json from get route

--- router/routes.js
+++ router/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('hello world');
+  res.json('hello world');
 });
 
 module.exports = router;
