f6f4574376d0a02461348f7607508c083c50cda2
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 12:49:52 -0600

    refactor configEnv const

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile');
-const configEnv = config[environment];
+const configEnv = knexConfig[environment];
 const knex = require('knex');
 const connection = knex(configEnv);
 
