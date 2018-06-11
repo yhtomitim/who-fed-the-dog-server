44dd3ca4ae57572bf214cc0afeec543137dc47a5
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 15:09:50 -0600

    add seed pet data

--- seeds/01_pet.js
+++ seeds/01_pet.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex('pet').del()
+    .then(() => {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('pet').insert([
+        {id: 1, petName: 'Snuggs'},
       ]);
     });
 };
