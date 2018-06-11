1c28716ccff633208fad98df21b6fd00e24563fa
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-11 14:02:53 -0600

    create migration for fed log with foreign keys, might be wrong schema

--- migrations/20180611134925_fed_log.js
+++ migrations/20180611134925_fed_log.js
@@ -1,8 +1,13 @@
 
-exports.up = function(knex, Promise) {
-  
+module.exports.up = (knex, Promise) => {
+  return knex.schema.createTable('fed', (table) => {
+    table.foreign('id').references('user.user_id_in_user');
+    table.foreign('id').references('pet.pet_id_in_pet');
+    table.dateTime('fedOn');
+
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+module.exports.down = (knex, Promise) => {
+  return knex.scheme.dropTableIfExists('fed');
 };
