fbb5191f9e10dd672f127b521263cbda10d85671
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-06-14 13:47:19 -0600

    create 201 status message for front end for post and delete routes

--- router/routes.js
+++ router/routes.js
@@ -4,58 +4,39 @@ const queries = require('../queries');
 
 router.get('/users', (req, res, next) => {
   queries.listUsers()
-    .then(users => { res.json({ users }); })
+    .then(users => res.json({ users }))
     .catch(next);
 });
 
 router.get('/users/:username', (req, res, next) => {
   queries.findUser(req.params)
-    .then(user => {
-      res.json({
-        user
-      });
-    })
+    .then(user => res.json({ user }))
     .catch(next);
 });
 
 router.get('/pets', (req, res, next) => {
   queries.listPets()
-    .then(pets => {
-      res.json({
-        pets
-      });
-    })
+    .then(pets => res.json({ pets }))
     .catch(next);
 });
 
 router.get('/pets/:pet', (req, res, next) => {
-  console.log(req.params);
-  
   queries.findPet(req.params)
-    .then(pet => {
-      res.json({
-        pet
-      });
-    })
+    .then(pet => res.json({pet}))
     .catch(next);
 });
 
 router.get('/fedon', (req, res, next) => {
   queries.listFedOn()
-    .then(fed => {
-      res.json({
-        fed
-      });
-    })
+    .then(fed => res.json({fed}))
     .catch(next);
 });
 
-// might have to do some sort of .innerJoin in the query
 router.post('/feed', (req, res, next) => {
   queries.createFeed(req.body)
     .then(console.log(req.body))
     .then(record => {
-      res.status(201).json({ record })
+      res.status(201).json({ message: 'Great Job!' });
     })
     .catch(next);
     
@@ -63,29 +44,27 @@ router.post('/feed', (req, res, next) => {
 
 router.post('/newuser', (req, res, next) => {
   queries.createUser(req.body)
-    .then(console.log(req.body))
-    .then(record => res.status(201).json(record))
+    .then(record => res.status(201).json({message: 'user created'}))
     .catch(next);
 });
 
 router.post('/newpet', (req, res, next) => {
   queries.createPet(req.body)
-    .then(console.log(req.body))
-    .then(record => res.status(201).json(record))
+    .then(record => res.status(201).json({message: 'pet created'}))
     .catch(next);
 });
 
 router.delete('/removepet/:pet', (req, res, next) => {
   queries.deletePet(req.body)
     .then(console.log(req.body))
-    .then(record => res.status(201).json(record))
+    .then(record => res.status(201).json({message: 'pet removed'}))
     .catch(next);
 });
 
 router.delete('/removeuser/:user', (req, res, next) => {
   queries.deleteUser(req.body)
     .then(console.log(req.body))
-    .then(record => res.status(201).json(record))
+    .then(record => res.status(201).json({message: 'user removed'}))
     .catch(next);
 });
 
